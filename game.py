8dcf033fb82aed585a0887c4c257ce9456a38bf6
SecuredAnomaly <114050514+SecuredAnomaly@users.noreply.github.com>
2022-09-28 09:57:07 +0300

    counts how many time you've played

--- game.py
+++ game.py
@@ -1,41 +1,32 @@
+import random
+turns = ['rock', 'paper', 'scissors']
+human_turns = []
+computer_turns = []
 
-import random    
+while(True):
+    human_turn = input("Enter your turn, or type exit: ")
+    computer_turn = random.choice(turns)
 
-
-possible_actions = ["rock", "paper", "scissors"]    
-
-while (True):
-    computer_action = random.choice(possible_actions)
-    user_action = input("Enter rock, paper, scissors:\n")  
-
-    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")   
-
-    if user_action == computer_action:                                     
-        print(f"Both players selected {computer_action}. It's a tie!")      
-    elif user_action == "rock":
-        if computer_action == "scissors":
-            print("Rock destroys scissors! You win!")
-        else:
-            print("Paper wraps rock! You lose")
-    elif user_action == "paper":
-        if computer_action == "rock":
-            print("Paper covers rock! you win")
-        else:
-            print("Scissors cuts paper you lose")
-    elif user_action == "scissors":
-        if computer_action == "paper":
-            print("Scissors snips paper! you win")
-    elif user_action == "your mom":
-        print("Your mother is so fat, king kong couldn't lift her")        
-    else:
-
-     while True:
-        answer = str(input('Atkal? (y/n): '))
-        if answer in ('y', 'n'):
-            break
-        print("iemācies rakstīt")
-    if answer == 'y':
-        continue
-    else:
-        print("Atā")
+    if human_turn == 'exit':
+        print('Thank you for playing! Bye bye')
         break
+
+    human_turns.append(human_turn)
+    computer_turns.append(computer_turn)
+
+    print(f'Human:{human_turn} vs. Computer:{computer_turn}')
+    if human_turn == computer_turn:
+        print('Draw!')
+    elif human_turn == 'rock' and computer_turn == 'scissors':
+        print('Human wins!')
+    elif human_turn == 'paper' and computer_turn == 'rock':
+        print('Human wins!')
+    elif human_turn == 'scissors' and computer_turn == 'paper':
+        print('Human wins!')
+    else:
+        print('Computer wins!')
+
+
+print(f'You have played {len(human_turns)} times')
+print(human_turns)
+print(computer_turns)
