9c495f2fdff30e9e7accd559a094a57de97bdc70
SecuredAnomaly <114050514+SecuredAnomaly@users.noreply.github.com>
2022-09-28 08:17:50 +0300

    Game repeats
    repeats with opthin y/n

--- game.py
+++ game.py
@@ -1,25 +1,41 @@
-import random       #random 
 
-user_action = input("Enter rock, paper, scissors:")  #izvēle
-possible_actions = ["rock", "paper", "scissors"]     #vērtības
-computer_action = random.choice(possible_actions)    #ran gen for computer_action
+import random    
 
-print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")    #info text
 
-if user_action == computer_action:                                      #rezultāti
-    print(f"Both players selected {computer_action}. It's a tie!")      
-elif user_action == "rock":
-    if computer_action == "scissors":
-        print("Rock destroys scissors! You win!")
+possible_actions = ["rock", "paper", "scissors"]    
+
+while (True):
+    computer_action = random.choice(possible_actions)
+    user_action = input("Enter rock, paper, scissors:\n")  
+
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")   
+
+    if user_action == computer_action:                                     
+        print(f"Both players selected {computer_action}. It's a tie!")      
+    elif user_action == "rock":
+        if computer_action == "scissors":
+            print("Rock destroys scissors! You win!")
+        else:
+            print("Paper wraps rock! You lose")
+    elif user_action == "paper":
+        if computer_action == "rock":
+            print("Paper covers rock! you win")
+        else:
+            print("Scissors cuts paper you lose")
+    elif user_action == "scissors":
+        if computer_action == "paper":
+            print("Scissors snips paper! you win")
+    elif user_action == "your mom":
+        print("Your mother is so fat, king kong couldn't lift her")        
     else:
-        print("Paper wraps rock! You lose")
-elif user_action == "paper":
-    if computer_action == "rock":
-        print("Paper covers rock! you win")
+
+     while True:
+        answer = str(input('Atkal? (y/n): '))
+        if answer in ('y', 'n'):
+            break
+        print("iemācies rakstīt")
+    if answer == 'y':
+        continue
     else:
-        print("Scissors cuts paper you lose")
-elif user_action == "scissors":
-    if computer_action == "paper":
-        print("Scissors snips paper! you win")
-    else:
-        print("Rock destroys scissors! you lose")
+        print("Atā")
+        break
